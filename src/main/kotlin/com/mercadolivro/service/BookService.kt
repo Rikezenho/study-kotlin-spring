e320fb76cb1dfe6a72627409126227d28deeb441
Henrique Kuwai <rikezenho@gmail.com>
2021-06-28 01:40:13 -0300

    Create ControllerAdvice

--- src/main/kotlin/com/mercadolivro/service/BookService.kt
+++ src/main/kotlin/com/mercadolivro/service/BookService.kt
@@ -25,7 +25,7 @@ class BookService(
     }
 
     fun findById(id: Int): BookModel {
-        return bookRepository.findById(id).orElseThrow()
+        return bookRepository.findById(id).orElseThrow { Exception("Não existe esse recurso") }
     }
 
     fun delete(id: Int) {
