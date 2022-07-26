8865057edfc73cc6b05e0284da7662c4b5107f23
Henrique <rikezenho@gmail.com>
2022-07-26 00:06:54 -0300

    Fix securityConfig matchers and cors

--- src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -34,8 +34,12 @@ class SecurityConfig(
 
     private val PUBLIC_MATCHERS = arrayOf<String>()
 
+    private val PUBLIC_GET_MATCHERS = arrayOf(
+        "/books"
+    )
+
     private val PUBLIC_POST_MATCHERS = arrayOf(
-        "/customer"
+        "/customers"
     )
 
     private val ADMIN_MATCHERS = arrayOf(
@@ -49,6 +53,7 @@ class SecurityConfig(
         http.cors().and().csrf().disable()
         http.authorizeRequests()
             .antMatchers(*PUBLIC_MATCHERS).permitAll()
+            .antMatchers(HttpMethod.GET, *PUBLIC_GET_MATCHERS).permitAll()
             .antMatchers(HttpMethod.POST, *PUBLIC_POST_MATCHERS).permitAll()
             .antMatchers(*ADMIN_MATCHERS).hasAuthority(Role.ADMIN.description)
             .anyRequest()
@@ -90,7 +95,7 @@ class SecurityConfig(
         val source = UrlBasedCorsConfigurationSource()
         val config = CorsConfiguration()
         config.allowCredentials = true
-        config.addAllowedOrigin("*")
+        config.addAllowedOriginPattern("*")
         config.addAllowedHeader("*")
         config.addAllowedMethod("*")
         source.registerCorsConfiguration("/**", config)
