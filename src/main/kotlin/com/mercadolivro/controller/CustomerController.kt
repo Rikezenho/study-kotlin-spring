780eed667e2ac195770c34b14382df2084e5b312
Henrique <rikezenho@gmail.com>
2022-07-25 23:03:55 -0300

    Add custom annotation and filters for admin and self-id

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -3,13 +3,16 @@ package com.mercadolivro.controller
 import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.controller.response.CustomerResponse
+import com.mercadolivro.enums.Role
 import com.mercadolivro.extension.toCustomerModel
 import com.mercadolivro.extension.toResponse
+import com.mercadolivro.security.UserCanOnlyAccessTheirOwnResource
 import com.mercadolivro.service.CustomerService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
@@ -19,11 +22,13 @@ class CustomerController(
     val customerService: CustomerService
 ) {
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     fun getAll(@RequestParam name: String?, @PageableDefault(page = 0, size = 10) pageable: Pageable): Page<CustomerResponse> {
         return customerService.getAll(name, pageable).map { it.toResponse() }
     }
 
     @GetMapping("/{id}")
+    @UserCanOnlyAccessTheirOwnResource
     fun get(@PathVariable id: Int): CustomerResponse? {
         return customerService.findById(id).toResponse()
     }
@@ -35,6 +40,7 @@ class CustomerController(
     }
 
     @PutMapping("/{id}")
+    @UserCanOnlyAccessTheirOwnResource
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun update(@PathVariable id: Int, @RequestBody @Valid customer: PutCustomerRequest) {
         val customerSaved = customerService.findById(id)
@@ -42,6 +48,7 @@ class CustomerController(
     }
 
     @DeleteMapping("/{id}")
+    @UserCanOnlyAccessTheirOwnResource
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun remove(@PathVariable id: Int) {
         customerService.remove(id)
