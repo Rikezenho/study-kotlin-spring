96d71f80a0dd9a08d381d1b2a46c19cbb52c92ef
Henrique <rikezenho@gmail.com>
2022-08-17 00:13:46 -0300

    Add jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.4.32"
 	kotlin("plugin.spring") version "1.4.32"
 	kotlin("plugin.jpa") version "1.4.32"
+	jacoco
 }
 
 group = "com.mercadolivro"
@@ -33,6 +34,7 @@ dependencies {
 	runtimeOnly("mysql:mysql-connector-java")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.12.0")
 }
 
 tasks.withType<KotlinCompile> {
